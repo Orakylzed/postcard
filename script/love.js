3f3e0192b1c68895a9c6f28e819762c6a42ee9b2
Orakylzed <62772548+Orakylzed@users.noreply.github.com>
2025-02-13 14:10:15 +0200

    Update love.js

--- script/love.js
+++ script/love.js
@@ -28,8 +28,8 @@ function init() {
   var koef = mobile ? 0.5 : 1;
   var canvas = document.getElementById("heart");
   var ctx = canvas.getContext("2d");
-  var width = (canvas.width = koef * innerWidth);
-  var height = (canvas.height = koef * innerHeight);
+  var width = (canvas.width = (koef * innerWidth)/0.5);
+  var height = (canvas.height = (koef * innerHeight)/0.5);
   var rand = Math.random;
 
   ctx.fillStyle = "rgba(0,0,0,1)";
